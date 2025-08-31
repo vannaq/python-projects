fe8b23c9ee45ac1d95c9076b8f31820ec5185307
vannaq <vanna.quon@gmail.com>
2025-08-31 02:51:54 -0400

    added netflix movie analysis project from DataCamp

---
+++ datacamp/basic_projects/netflix_movies_analysis/README.md
@@ -0,0 +1,17 @@
+You work for a production company that specializes in nostalgic styles, and want to do some research on movies released in the 1990's. You'll delve into Netflix data and perform exploratory data analysis to better understand this awesome movie decade!
+
+netflix_data.csv contains the following:
+
+| Column        |    Description                    |
+|-------------- |---------------------------------- |
+`'show_id'`	    |   The ID of the show              |
+`'type'`        |   Type of show                    |
+`'title'`       |	Title of the show               |
+`'director'`    | 	Director of the show            |
+`'cast'`        | 	Cast of the show                |
+`'country'`	    |   Country of origin               |
+`'date_added'`	|   Date added to Netflix           |
+`'release_year'`|   Year of Netflix release         |
+`'duration'`	|   Duration of the show in minutes |
+`'description'`	|   Description of the show         |
+`'genre'`	    |   Show genre                      |
---
+++ datacamp/basic_projects/netflix_movies_analysis/netflix_analysis.py
@@ -0,0 +1,30 @@
+#Importing pandas and matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Read in the Netflix CSV as a DataFrame
+netflix_df = pd.read_csv("datacamp/basic_projects/netflix_movies_analysis/netflix_data.csv", index_col=0)       #index_col=0 removes the indexing column
+
+
+#Filter for Movies only
+movies_only = netflix_df[netflix_df["type"] == "Movie"]
+
+
+#Filter for Movies released between 1990 to 1999
+movies_1990s = netflix_df[(netflix_df["release_year"] >= 1990) & (netflix_df["release_year"] < 2000)]
+
+
+#Find most frequent movie duration in 1990s
+duration = movies_1990s["duration"].mode().iloc[0]                                      #.mode() returns the most frequent value(s) in a column so if there's only one most frequent value, it'll return a series with 1 row. If there's a tie, it'll return multiple values. Without .iloc[0] the result will be two values where the first value is 0, which is the index of the series (not a value).
+print(f"The most frequent movie duration in the 1990s was {duration} minutes.")
+
+
+#Filter for short action movie genre, which is less than 90 minutes long
+short_action_movie = movies_1990s[(movies_1990s["duration"] < 90) & (movies_1990s["genre"] == "Action")]
+
+
+short_movie_count = len(short_action_movie)
+print(f"The number of short action movies released in 1990s is {short_movie_count}.")
+ 
+
+
