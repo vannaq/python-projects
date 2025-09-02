13eadb2b2867da06f8e9a9125b9f0c1b05543594
vannaq <vanna.quon@gmail.com>
2025-09-01 22:05:12 -0400

    clarified comments to work with dataframe or series
    - added histogram to view movie duration data
    - histogram includes axis labels

--- datacamp/basic_projects/netflix_movies_analysis/netflix_analysis.py
+++ datacamp/basic_projects/netflix_movies_analysis/netflix_analysis.py
@@ -6,25 +6,31 @@ import matplotlib.pyplot as plt
 netflix_df = pd.read_csv("datacamp/basic_projects/netflix_movies_analysis/netflix_data.csv", index_col=0)       #index_col=0 removes the indexing column
 
 
-#Filter for Movies only
+#Filter dataframe for Movies only
 movies_only = netflix_df[netflix_df["type"] == "Movie"]
 
 
-#Filter for Movies released between 1990 to 1999
+#Filter dataframe for Movies released between 1990 to 1999
 movies_1990s = netflix_df[(netflix_df["release_year"] >= 1990) & (netflix_df["release_year"] < 2000)]
 
+#View single column of dataframe, also known as a series, in histogram
+movies_1990s["duration"].hist(bins=20)
+plt.title("Distribution of Movie Durations in the 1990s")
+plt.xlabel("Duration(mins)")
+plt.ylabel("Number of Movies")
+plt.show()
 
 #Find most frequent movie duration in 1990s
 duration = movies_1990s["duration"].mode().iloc[0]                                      #.mode() returns the most frequent value(s) in a column so if there's only one most frequent value, it'll return a series with 1 row. If there's a tie, it'll return multiple values. Without .iloc[0] the result will be two values where the first value is 0, which is the index of the series (not a value).
 print(f"The most frequent movie duration in the 1990s was {duration} minutes.")
 
 
-#Filter for short action movie genre, which is less than 90 minutes long
-short_action_movie = movies_1990s[(movies_1990s["duration"] < 90) & (movies_1990s["genre"] == "Action")]
+#Filter for short action movies genre, which is less than 90 minutes long
+short_action_movies = movies_1990s[(movies_1990s["duration"] < 90) & (movies_1990s["genre"] == "Action")]
 
 #Using len function, we can find the number of items in the filtered series from the previous step
-short_movie_count = len(short_action_movie)
-print(f"The number of short action movies released in 1990s is {short_movie_count}.")
+short_movies_count = len(short_action_movies)
+print(f"The number of short action movies released in 1990s is {short_movies_count}.")
 
 
 # While the following section of code works, it is slow and more memory-intensive than vectorized pandas filtering.
@@ -47,7 +53,4 @@ shorts_df = netflix_df[(netflix_df["duration"] < 70) & (netflix_df["genre"] == "
 
 #Export to csv and save to location 
 shorts_df.to_csv('datacamp/basic_projects/netflix_movies_analysis/shorts.csv', index=False)
-
- 
-
 
