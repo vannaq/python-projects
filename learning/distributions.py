d67504a4efc1e04c6998d5dfdc7ff8dac4eae61a
vannaq <vanna.quon@gmail.com>
2025-09-13 17:10:49 -0400

    added notes for discrete, uniform, and binomial distributions

--- learning/distributions.py
+++ learning/distributions.py
@@ -9,3 +9,11 @@
 ##probabilities in between 2 values: uniform.cdf(upper value, LL, UL) - uniform.cdf(lower value, LL, UL)
 
 ##generating random numbers according to uniform distribution: uniform.rvs(min value, max value, size=random number of values to generate)
+
+
+#binomial distributions#
+#binomial distribution is a probability distribution of the number of successes in a sequence of independent trials
+#from scipy.stats import binom
+#random variates for generating random numbers from a distribution: binom.rvs(# of trials, probability of success, size=# of trials)
+#probability mass function gives probability of a discrete random variable equalling a specific value:binom.pmf(# of successes, # of trials, probability of success)
+#cumulative distribution function gives probability of a random variable being less than or equal to a value: binom.cdf(# of successes, # of trials, probability of success)
