007d75f9a92a938f50bbb0728bc52f7e7093c52f
vannaq <vanna.quon@gmail.com>
2025-09-26 00:10:28 -0400

    added notes about seaborn basics

--- learning/seaborn.py
+++ learning/seaborn.py
@@ -17,4 +17,11 @@
 #using DataFrames with countplot()
 #import pandas, matplotlib, and seaborn
 #create a pandas DataFrame from csv file to create the countplot instead of a list of data
-#
+#sns.countplot(x="name_of_column_in_df", data=df)
+#plt.show()
+#Note: Seaborn works great with pandas DataFrames only if the DataFrame is "tidy", meaning each observation has its own row & each variable has its own column
+#add color using seaborn's hue argument: hue="smoker"
+#set hue order: hue_order=["value", "value2"]
+#map hue color using key value: hue_colors={"variable":"color", "Variable2":"color2"}
+#adding subgroups: hue="subgroup_name"
+#specifying subgroup colors: palette=palette_colors where palette_colors = {"variable": "color"}
