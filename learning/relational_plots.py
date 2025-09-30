d0728419e7a1b4d0141ded599b1f91a9d4a8e29f
vannaq <vanna.quon@gmail.com>
2025-09-30 02:00:49 -0400

    learning notes for seaborn relplot

---
+++ learning/relational_plots.py
@@ -0,0 +1,5 @@
+#Seaborn function relplot() stands for relational plot, which enables visualizing the relationship between 2 quantitative variables using either scatter plots or line plots
+#advantage over scatterplot() is that it can create subplots in a single figure
+#sns.relplot(x="x_axis_variable", y="y_axis_variable", data=dataset, kind="scatter_or_line", col="col_category", row="row_category")
+#col_wrap parameter sets how many plots per row before it goes to next row
+#col_order and row_order parameters allow for changes to the ordering of subplots based on a list of ordered values
