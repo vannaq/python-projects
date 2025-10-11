53f647d8ec2f0bfca773a7255e0701aab4cd5b96
vannaq <vanna.quon@gmail.com>
2025-10-11 02:30:01 -0400

    added notes for relational plots and seaborn categorical plots

--- learning/relational_plots.py
+++ learning/relational_plots.py
@@ -3,3 +3,8 @@
 #sns.relplot(x="x_axis_variable", y="y_axis_variable", data=dataset, kind="scatter_or_line", col="col_category", row="row_category")
 #col_wrap parameter sets how many plots per row before it goes to next row
 #col_order and row_order parameters allow for changes to the ordering of subplots based on a list of ordered values
+
+#when creating a scatter plot, control the size of points using the size parameter: sns.relplot(x="x_axis_name", y="y_axis_name", data=datasetname, kind="scatter", size="parameter_for_size")
+#control the color of point styles with hue parameter: sns.relplot(x="x_axis_name", y="y_axis_name", data=datasetname, kind="scatter", hue="parameter_for_color")
+#setting a point style allows for better differentiation between subgroups: style="parameter_For_style"
+#point transparency is set with the alpha parameter, which is a value between 0 and 1: alpha=0.4
--- learning/seaborn.py
+++ learning/seaborn.py
@@ -25,3 +25,19 @@
 #map hue color using key value: hue_colors={"variable":"color", "Variable2":"color2"}
 #adding subgroups: hue="subgroup_name"
 #specifying subgroup colors: palette=palette_colors where palette_colors = {"variable": "color"}
+
+#In a line plot, each plot point represents the same "thing" typically tracked over time whereas each point in a scatter plot is an independent observation
+#To visualize subgroups: kind="line", style="parameter", hue="parameter"
+#Markers parameter set to True will display a marker for each data point: markers=True
+#To remove line style for subgroups, set dashes parameter to false: dashes=False
+#Given multiple observations per x axis value, seaborn will show aggregate value (Defaults to mean) and the shaded region is the confidence interval
+#Use ci parameter to see standard deviation or turn it off: ci="sd" or ci=None
+
+#categorical plots commonly used when making comparisons between different groups: count plots, bar plots
+#catplot() used to create categorical plots, with the same flexibility and advantages of relplot, such as col and row paramters
+#to create catplot: sns.catplot(x="x_axis", data=dataname, kind="count")
+#to change order, list them in desired order before calling seaborn plot and include order parameter. works for all types of categorical plots: category_order = ["item1", "item2", "item3", "item4", "item5"] then sns.catplot(x="x_axis", y="y_axis", data=dataname, kind="bar", order=category_order)
+
+#bar plots display mean of quantitative variable per category: sns.catplot(x="x_axis", y="y_axis", data=dataname, kind="bar")
+#seaborn automatically shows 95% confidence intervals for mean so assuming data is a random sample, we're 95% sure the true population mean in each group lies within the confidence interval shown. To no longer display confidence interval: ci=None
+#to change orientation of bar plot, swap x and y axis: sns.catplot(y="previous_x_axis_name", data=dataname, kind="bar")
